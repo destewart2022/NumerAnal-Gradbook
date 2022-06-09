47b1f53e94a26d26d7f856fe6fe1859a8bfb5149
David Stewart <dstewart7309@gmail.com>
2022-06-09 15:18:42 -0500

    Updated to more modern style

--- Matlab/solvers/newton.m
+++ Matlab/solvers/newton.m
@@ -1,22 +1,22 @@
 function x = newton(func,dfunc,x,eps,trace)
-% newton(FUNC,DFUNC,X,EPS,TRACE)
+% newton(func,dfunc,x,eps,trace)
 % Performs one step of Newton's method from X.
-%   FUNC   -- function name
-%   DFUNC  -- gradient function name
-%   EPS    -- stopping tolerance
-%   TRACE  -- set to non-zero for trace of execution
-% Returns the new value X
-  fx = feval(func,x);
+%   func   -- function to solve func(x) = 0
+%   dfunc  -- derivative/Jacobian function 
+%   eps    -- stopping tolerance
+%   trace  -- set to non-zero for trace of execution
+% Returns the new value x
+  fx = func(x);
   iter = 0;
   while norm(fx,2) >= eps
-    dfx = feval(dfunc,x);
+    dfx = dfunc(x);
     if trace
       fprintf('Iter # %d: [x,fx] = ',iter);
       [x,fx]
       norm_fx = norm(fx)
     end
     x = x - dfx\fx;
-    fx = feval(func,x);
+    fx = func(x);
     iter = iter + 1;
   end
   if trace
